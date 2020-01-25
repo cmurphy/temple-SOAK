65373e668ca3ce86937268269daf792e89740716
Colleen Murphy <colleen@gazlene.net>
2020-01-25 14:00:21 -0800

    Add second stage of sunset show

--- temple.ino
+++ temple.ino
@@ -6,6 +6,13 @@
 #define NUM_RINGS 5
 CRGBArray<NUM_LEDS> leds;
 
+DEFINE_GRADIENT_PALETTE(twilightPalette) {
+      0,   0, 171,  85, // aqua
+     85,   0,   0, 255, // blue
+    170,  85,   0, 171, // purple
+    255, 171,   0,  85, // pink
+};
+
 void setup() {
     FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
     FastLED.setBrightness(125);
@@ -53,6 +60,24 @@ void sunset() {
             } else {
                 brightness += 2;
             }
+            break;
+        case 1: // map to twilight colors, aqua through pink
+            // https://pastebin.com/r70Qk6Bn
+            static CRGBPalette16 currentPalette(CHSV(160, 255, 255));
+            static CRGBPalette16 targetPalette(twilightPalette);
+            if (currentPalette == targetPalette) {
+                stage++;
+            }
+            EVERY_N_MILLIS(40) {
+                nblendPaletteTowardPalette(currentPalette, targetPalette, 24);
+            }
+            #define scale 30
+            static uint16_t dist;
+            for (int i = 0; i < NUM_LEDS; i++) {
+                uint8_t index = inoise8(i*scale, dist+i*scale);
+                leds[i] = ColorFromPalette(currentPalette, index, 255, LINEARBLEND);
+            }
+            dist += beatsin8(10, 1, 4);
             break;
         default:
             hsv.hue = 160;
