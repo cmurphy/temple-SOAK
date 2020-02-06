ad425d618ee539a4943add32b3c6486416d60680
Colleen Murphy <colleen@gazlene.net>
2020-02-05 22:27:48 -0800

    Add fire effect action

--- temple.ino
+++ temple.ino
@@ -364,5 +364,53 @@ char actionA() {
     return ActionA;
 }
 
-char actionB() {Serial.println("action b");}
+// Fire effect
+// https://github.com/FastLED/FastLED/blob/dcbf39933f51a2a0e4dfa0a2b3af4f50040df5c9/examples/Fire2012/Fire2012.ino
+char actionB() {
+    static char    timeout = ACTION_TIMEOUT;
+    EVERY_N_SECONDS(1) {
+        timeout--;
+    }
+
+    // Array of temperature readings at each simulation cell
+    static byte heat[NUM_LEDS];
+    // COOLING: How much does the air cool as it rises?
+    // Less cooling = taller flames.  More cooling = shorter flames.
+    // Default 50, suggested range 20-100
+    #define COOLING  55
+
+    // SPARKING: What chance (out of 255) is there that a new spark will be lit?
+    // Higher chance = more roaring fire.  Lower chance = more flickery fire.
+    // Default 120, suggested range 50-200.
+    #define SPARKING 120
+    // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+        heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+        heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+        int y = random8(7);
+        heat[y] = qadd8( heat[y], random8(160,255) );
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+        leds[j] = HeatColor( heat[j]);
+    }
+
+    FastLED.show();
+    FastLED.delay(16);
+    if (!timeout) {
+        timeout = ACTION_TIMEOUT;
+        return 0;
+    }
+    return ActionB;
+}
+
 char actionC() {Serial.println("action c");}
